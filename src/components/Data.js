27ca4fbc2b356e60bb1aca373248ee866a257e4d
coderealistics <coderealistics@yandex.com>
2020-12-05 10:15:00 +0500

    first commit

---
+++ src/components/Data.js
@@ -0,0 +1,14 @@
+export const Data = [
+    {
+        question: "What do you call a dog magician?",
+        answer: "A labracabrador."
+    },
+    {
+        question: "What do you call a dog magician?",
+        answer: "A labracabrador."
+    },
+    {
+        question: "What do you call a dog magician?",
+        answer: "A labracabrador."
+    },
+]
